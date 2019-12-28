5044385b2bb3f89b2d91e9180884abe15fffa331
bomko <dejan.obrez@gmail.com>
2019-12-28 08:17:03 +0100

    added redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 //import logo from './logo.svg';
 import './App.css';
 import CardList from '../components/CardList';
@@ -6,57 +7,59 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 //import {robots} from './robots'
+import {setSearchField, requestRobots} from '../actions';
 
+//mapStateToProps, mapDispatchToProps
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    //onRequestRobots: () => requestRobots(dispatch)
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+  
+}
 
 class App extends React.Component {
+  //tega ne rabimo vec, ker nimamo vec stateov. robote bomo dobil iz propov -> onRequestRobots()
+  /*
   constructor(){
     super();
     this.state = {
-      robots: [],
-      searchfield: ''
+      robots: []
     }
     console.log('constructor');
   }
-
+  */
   componentDidMount(){
-    
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=>{
-        return response.json();
-      })
-      .then(users=>{
-        this.setState({robots:users})
-      })
-      
-    
-      ///krajse
-      /*
-      fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=>response.json())
-      .then(users=>this.setState({robots:users}))
-      */
-    //this.setState({robots:robots});
+    this.props.onRequestRobots();
     console.log('componentDidMount');
-  }
-
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value});
   }
 
   render() {
     //zarad tega ni treba zdej ppovsod pisat this.state.searchfield.toLowerCase()
-    const{robots, searchfield} = this.state
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+
+    const {searchField, onSearchChange, robots, isPending} = this.props;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     console.log('render');
-    if (robots.length === 0){
+    if (isPending){
       return <h1>Loading</h1>
     }else{
       return(
         <div className='tc'>
           <h1 className='f1'>Robofriends</h1>  
-          <SearchBox searchChange = {this.onSearchChange}/>
+          <SearchBox searchChange = {onSearchChange}/>
           <Scroll>
             <ErrorBoundry>
               <CardList robots = {filteredRobots}/>
@@ -123,4 +126,4 @@ function App() {
   );
 }
 */
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
